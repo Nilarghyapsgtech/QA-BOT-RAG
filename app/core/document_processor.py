42539053d45a341d59593e40e219f63d1bbd82c3
Nilarghya Sarkar <21z372csepsgtech@gmail.com>
2026-07-06 12:12:48 +0530

    Add Document Splitter

--- app/core/document_processor.py
+++ app/core/document_processor.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import (
     PyPDFLoader,
     TextLoader
 )
-from typing import BinaryIO
+from typing import BinaryIO,List
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pathlib import Path
 from langchain_core.documents import Document
@@ -23,7 +23,7 @@ class DocumentProcessor:
         self.chunk_size=chunk_size or settings.chunk_size
         self.chunk_overlap=chunk_overlap or settings.chunk_overlap
 
-        textsplitter=RecursiveCharacterTextSplitter(
+        self.textsplitter=RecursiveCharacterTextSplitter(
             chunk_size=chunk_size,
             chunk_overlap=chunk_overlap,
             separators=["\n\n", "\n", " ", ""],
@@ -60,8 +60,55 @@ class DocumentProcessor:
 
         logger.info(f"Loaded {len(documents)} rows from {file_path.name}")
         return documents
+    
+    def load_file(self,file_path:str|Path)->list[Document]:
+        file_path=Path(file_path)
+        extension=Path(file_path).suffix.lower()
+        if extension not in self.SUPPORTED_EXTENSIONS:
+            raise ValueError(f"Unsupported File type .Supported filetypes are {self.SUPPORTED_EXTENSIONS}")
+        loaders={
+             ".pdf": self.load_pdf,
+            ".txt": self.load_text,
+            ".csv": self.load_csv,
+        }
+        loaders[extension](str(file_path))
 
+    def load_from_upload(self,file:BinaryIO,filename:str)->list[Document]:
+        extension=Path(filename).suffix.lower()
 
+        if extension not in self.SUPPORTED_EXTENSIONS:
+            raise ValueError(f"Unsupported File type .Supported filetypes are {self.SUPPORTED_EXTENSIONS}")
+        
+        with tempfile.NamedTemporaryFile(
+            delete=False,
+            suffix=extension
+        ) as temp_file:
+            temp_file.write(file.read())
+            temp_path=temp_file.name
+
+        try:
+            documents=self.load_file(temp_path)
+            for doc in documents:
+                doc.metadata["source"]=filename
+
+            return documents
+        finally:
+            Path(temp_path).unlink(missing_ok=True)
+
+    def split_documents(self,documents:list[Document])->list[Document]:
+
+        logger.info(f"Splitting {len(documents)} documents into chunks")
+        chunks=self.textsplitter.split_documents(documents)
+
+        return chunks
+    def process_file(self,file_path:str|Path)->list[Document]:
+        file_path=Path(file_path)
+        documents=self.load_file(str(file_path))
+        chunks=self.split_documents(documents)
+
+    def process_upload(self,file:BinaryIO,filename:str)->list[Document]:
+        documents=self.load_from_upload(file,filename)
+        return self.split_documents(documents)
 
 
 
